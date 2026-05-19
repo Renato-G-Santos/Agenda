9bacc1c3e36b027e4a7e882cf7ba5c6a73609d76
Renato-G-Santos <renatogomes1616@gmail.com>
2026-05-19 13:43:20 -0300

    pagina de erro rest_framework

--- agenda/views.py
+++ agenda/views.py
@@ -3,15 +3,17 @@ from django.shortcuts import get_object_or_404
 from .models import agendamento
 from .serializes import AgendamentoSerializer
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
 
 # Create your views here.
 
-
+@api_view(['GET'])
 def agendamento_detail(request, id):
     obj = get_object_or_404(agendamento, id=id)
     serializer = AgendamentoSerializer(obj)
     return JsonResponse(serializer.data)
 
+@api_view(['GET'])
 def agendamento_list(request):
     qs = agendamento.objects.all()
     serializer = AgendamentoSerializer(qs, many=True)
